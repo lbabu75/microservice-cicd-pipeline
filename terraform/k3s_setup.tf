96aeb7935bdad6a64fc40386b94daaaaef447b45
lbabu75 <99012520+lbabu75@users.noreply.github.com>
2026-02-08 23:21:42 +0530

    Refactor K3s setup to use local-exec provisioner
    Replaced the k3s_bootstrap resource with k3s_install_local to streamline K3s installation and firewall configuration.

--- terraform/k3s_setup.tf
+++ terraform/k3s_setup.tf
@@ -1,23 +1,20 @@
-# terraform/k3s_setup.tf
-resource "null_resource" "k3s_bootstrap" {
-  triggers = {
-    public_ip = var.VM_PUBLIC_IP
-  }
+resource "null_resource" "k3s_install_local" {
+  # This runs only once unless you manually taint it
+  provisioner "local-exec" {
+    command = <<EOT
+      # 1. Open Firewall ports locally
+      sudo firewall-cmd --permanent --add-port=6443/tcp
+      sudo firewall-cmd --permanent --add-port=80/tcp
+      sudo firewall-cmd --permanent --add-port=443/tcp
+      sudo firewall-cmd --reload
 
-  connection {
-    type        = "ssh"
-    user        = "opc"
-    private_key = file(var.SSH_PRIVATE_KEY_PATH)
-    host        = self.triggers.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo firewall-cmd --permanent --add-port=6443/tcp",
-      "sudo firewall-cmd --permanent --add-port=80/tcp",
-      "sudo firewall-cmd --permanent --add-port=443/tcp",
-      "sudo firewall-cmd --reload",
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--tls-san ${self.triggers.public_ip}' sh -"
-    ]
+      # 2. Install K3s (using the VM's public IP for the certificate)
+      # We fetch the public IP dynamically so you don't have to hardcode it
+      PUBLIC_IP=$(curl -s ifconfig.me)
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--tls-san $PUBLIC_IP" sh -
+      
+      # 3. Fix permissions so the runner user can use kubectl
+      sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+    EOT
   }
 }
