57504da461f83e9bdbb6c14dc5903fee326797e5
lbabu75 <99012520+lbabu75@users.noreply.github.com>
2026-02-09 00:25:58 +0530

    Update K3s installation method to use public IP
    Replaced shell command for public IP retrieval with a variable assignment.

--- terraform/k3s_setup.tf
+++ terraform/k3s_setup.tf
@@ -10,9 +10,9 @@ resource "null_resource" "k3s_install_local" {
       sudo firewall-cmd --permanent --add-port=80/tcp
       sudo firewall-cmd --permanent --add-port=443/tcp
       sudo firewall-cmd --reload
-
-      # Use the Terraform data source variable here instead of a shell command
-      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--tls-san ${data.http.my_ip.response_body}" sh -
+      # Install K3s
+      PUBLIC_IP=$(curl -s ifconfig.me)
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--tls-san $PUBLIC_IP" sh -
       
       sudo chmod 644 /etc/rancher/k3s/k3s.yaml
     EOT
